b90d03b2b2cff294c42f8e2e82f5c9277d10068b
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-05-20 00:35:33 +0530

    Database Connect

--- App-01/src/main/java/lk/ijse/EventServlet.java
+++ App-01/src/main/java/lk/ijse/EventServlet.java
@@ -1,5 +1,6 @@
 package lk.ijse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,11 +8,51 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet("/event")
 public class EventServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //eid, ename, edescription, edate, eplace
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/event",
+                            "root",
+                            "Ijse@1234");
+
+            PreparedStatement pstm = connection.prepareStatement(
+                "select * from events"
+            );
+            ResultSet resultSet = pstm.executeQuery();
+            List<Map<String, String>> elist = new ArrayList<Map<String, String>>();
+
+            while (resultSet.next()) {
+                Map<String, String> event = new HashMap<String, String>();
+                event.put("eid", resultSet.getString("eid"));
+                event.put("ename", resultSet.getString("ename"));
+                event.put("edescription", resultSet.getString("edescription"));
+                event.put("edate", resultSet.getString("edate"));
+                event.put("eplace", resultSet.getString("eplace"));
+                elist.add(event);
+            }
+
+            resp.setHeader("Access-Control-Allow-Origin", "*");
+            resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            resp.setContentType("application/json");
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(resp.getWriter(), elist);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
