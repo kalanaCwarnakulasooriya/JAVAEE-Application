3f77aacc1ca44218bb182afcf323564246c5b6b4
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-05-26 20:32:24 +0530

    Query String Parameters Servlet

---
+++ App-01/src/main/java/lk/ijse/QueryStringParamServlet.java
@@ -0,0 +1,21 @@
+package lk.ijse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/queryString")
+public class QueryStringParamServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("eid");
+        String name = req.getParameter("ename");
+
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+    }
+}
