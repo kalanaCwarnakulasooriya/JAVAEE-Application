9a9e69b5edcc54a205e921ed5d348978d4f16cb1
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-05-26 11:10:53 +0530

    Load Table

--- App-01/src/main/java/lk/ijse/EventServlet.java
+++ App-01/src/main/java/lk/ijse/EventServlet.java
@@ -1,7 +1,6 @@
 package lk.ijse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,25 +15,24 @@ import java.util.Map;
 
 @WebServlet("/event")
 public class EventServlet extends HttpServlet {
+    private Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/event",
+                "root",
+                "Ijse@1234"
+        );
+    }
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //eid, ename, edescription, edate, eplace
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection =
-                    DriverManager.getConnection("jdbc:mysql://localhost:3306/event",
-                            "root",
-                            "Ijse@1234");
-
-            PreparedStatement pstm = connection.prepareStatement(
-                "select * from events"
-            );
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (Connection connection = getConnection()) {
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM events");
             ResultSet resultSet = pstm.executeQuery();
-            List<Map<String, String>> elist = new ArrayList<Map<String, String>>();
 
+            List<Map<String, String>> elist = new ArrayList<>();
             while (resultSet.next()) {
-                Map<String, String> event = new HashMap<String, String>();
+                Map<String, String> event = new HashMap<>();
                 event.put("eid", resultSet.getString("eid"));
                 event.put("ename", resultSet.getString("ename"));
                 event.put("edescription", resultSet.getString("edescription"));
@@ -43,16 +41,18 @@ public class EventServlet extends HttpServlet {
                 elist.add(event);
             }
 
-            resp.setHeader("Access-Control-Allow-Origin", "*");
-            resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            setCORS(resp);
             resp.setContentType("application/json");
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.writeValue(resp.getWriter(), elist);
+            new ObjectMapper().writeValue(resp.getWriter(), elist);
 
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
         }
+    }
+
+    private void setCORS(HttpServletResponse resp) {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
     }
 }
